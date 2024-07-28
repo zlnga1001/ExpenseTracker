1d482549a3d8c7486e410dde13b8315db2e1a4fa
DaZeTw <vanchuongno3@gmail.com>
2024-07-28 23:59:11 +0700

    Update backend

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const cors = require('cors');
-const { db } = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
+const { readdirSync } = require("fs");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
 const server = () => {
-    db()
-    app.listen(PORT, () => {
-        console.log('listening to port:', PORT)
-    })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log("listening to port:", PORT);
+  });
+};
 
-server()
+server();
